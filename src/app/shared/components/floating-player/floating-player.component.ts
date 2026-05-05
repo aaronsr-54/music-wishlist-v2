aaa2107cd0c97b64bb05aa731cda7a9dcb716b48
AaronSR-54 <a.sanzblog6b@gmail.com>
2026-05-05 16:13:57 +0200

    style: floatin player dark mode

--- src/app/shared/components/floating-player/floating-player.component.ts
+++ src/app/shared/components/floating-player/floating-player.component.ts
@@ -19,7 +19,7 @@ import { PreviewService } from '../../../core/services/preview.service';
   },
   template: `
     <div
-      class="flex items-center gap-3 p-2.5 rounded-xl bg-bone/95 backdrop-blur-sm shadow-lg overflow-hidden w-full"
+      class="flex items-center gap-3 p-2.5 rounded-xl bg-ink dark:bg-bone backdrop-blur-sm shadow-lg overflow-hidden w-full"
     >
       <app-cover
         [name]="metadata()?.title ?? ''"
@@ -29,10 +29,12 @@ import { PreviewService } from '../../../core/services/preview.service';
       />
 
       <div class="flex flex-col flex-1 min-w-0 pr-2 w-full">
-        <span class="text-sm font-medium text-ink truncate w-full">
+        <span
+          class="text-sm font-medium text-bone dark:text-ink truncate w-full"
+        >
           {{ metadata()?.title }}
         </span>
-        <span class="text-xs text-ink-600 truncate w-full">
+        <span class="text-xs text-bone-600 dark:text-ink-600 truncate w-full">
           {{ metadata()?.artist }}
         </span>
       </div>
@@ -43,12 +45,15 @@ import { PreviewService } from '../../../core/services/preview.service';
           class="w-8 h-8 flex items-center justify-center"
           title="Stop"
         >
-          <app-icon name="stop" class="w-8 h-8 fill-ink-200 hover:fill-ink" />
+          <app-icon
+            name="stop"
+            class="w-8 h-8 fill-bone-400 hover:fill-bone dark:fill-ink-200 hover:dark:fill-ink"
+          />
         </button>
 
         <button
           (click)="togglePlay()"
-          class="w-10 h-10 flex items-center justify-center rounded-full bg-ink text-bone hover:scale-105 active:scale-95 transition-transform"
+          class="w-10 h-10 flex items-center justify-center rounded-full bg-bone dark:bg-ink text-ink dark:text-bone hover:scale-105 active:scale-95 transition-transform"
           title="{{ isPlaying() ? 'Pause' : 'Play' }}"
         >
           <app-icon [name]="isPlaying() ? 'pause' : 'play'" class="w-5 h-5" />
@@ -57,9 +62,11 @@ import { PreviewService } from '../../../core/services/preview.service';
         <div
           class="absolute bottom-[2px] left-0 right-0 flex justify-center px-6"
         >
-          <div class="h-1 w-full rounded-full bg-bone-400/30 overflow-hidden">
+          <div
+            class="h-1 w-full rounded-full bg-ink-400/30 dark:bg-bone-400/30 overflow-hidden"
+          >
             <div
-              class="h-full rounded-full bg-ink-600 transition-all duration-100"
+              class="h-full rounded-full bg-bone dark:bg-ink transition-all duration-100"
               [style.width.%]="progress()"
             ></div>
           </div>
